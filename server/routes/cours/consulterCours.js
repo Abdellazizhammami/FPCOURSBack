9b4cf4dc9caaadae6d00426d79af1605bb7696b0
HAMMAMI Abdellaziz <abdellaziz.esti@gmail.com>
2019-02-21 18:29:42 +0100

    conscours

--- server/routes/cours/consulterCours.js
+++ server/routes/cours/consulterCours.js
@@ -3,9 +3,9 @@ const cours=require('../../models/cours');
 const verifytoken= require('./../jwt').verifyToken;
 
 
-router.get('/consulterArt/:idCours', verifytoken,async (req,res)=>{
+router.get('/consulterCours/:idCours', verifytoken,async (req,res)=>{
     try {
-        const resultat=await cours.findById(req.params.idCours).exec();
+        const resultat=await cours.findById(req.params.idCours).populate({path:'prof',select:['name','lastname']}).exec();
         res.send(resultat);
     } catch (error) {
         res.send('Identificateur de article est non valide, veuillez saisir un autre identifiant');
